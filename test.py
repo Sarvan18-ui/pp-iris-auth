8ab85de6383262232e836309c40df4888677f945
Sarvan <kumarsarvanjha2005@gmail.com>
2026-05-20 14:37:09 +0530

    Completed Week 1 - Dataset Exploration

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+import cv2
+from Crypto.Cipher import AES
+
+print("PyTorch version:", torch.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Everything working perfectly!")
